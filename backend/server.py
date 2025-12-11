1778c3c52323651a45f78ca3e8e20962c39b2e5c
bills <billsar1912@gmail.com>
2025-12-11 20:24:30 +0700

    'modify server.py to update allow_origins in CORS middleware and add google-generativeai to requirements.txt'

--- backend/server.py
+++ backend/server.py
@@ -295,7 +295,10 @@ app.include_router(auth_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://smart-se26-agentic-ai-chatbot-web.onrender.com",
+        "http://localhost:3000"
+        ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
